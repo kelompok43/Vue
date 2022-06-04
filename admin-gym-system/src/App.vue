8d6307c5c302541ed0b2981bd23d453861651a3e
vidityar adith <van.adith@gmail.com>
2022-06-04 20:53:18 +0700

    Feature Dashboard

--- admin-gym-system/src/App.vue
+++ admin-gym-system/src/App.vue
@@ -1,60 +1,139 @@
 <template>
   <v-app>
+    <v-navigation-drawer 
+      v-model="drawer"
+      fixed
+      app
+      color="#FEFEFE"
+    >
+      <v-list-item>
+        <div class="d-flex align-center pa-52px">
+          <v-list-item-content>
+            <v-img
+            alt="Gym Management Logo"
+            contain
+            src="@/assets/Logo.png"
+            height="127px"
+            width="212px"
+          />
+          </v-list-item-content>
+        </div>
+      </v-list-item>
+
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+          @click="press(item.title)"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    
     <v-app-bar
       app
-      color="primary"
-      dark
+      color="#FEFEFE"
+      fixed
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-app-bar-nav-icon 
+      @click.stop="drawer = !drawer"
+      color="#F48743">
+    </v-app-bar-nav-icon>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+      <div class="d-flex align-center">
+        <v-toolbar-title
+        class="font-weight-bold"
+        > {{this.page}} </v-toolbar-title>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn text
+          v-bind="attrs"
+          v-on="on">
+            <span class="mr-2">SuperAdmin</span>
+            <v-icon>mdi-account-circle</v-icon>
+          </v-btn>
+        </template>
+        
+        <v-list>
+          <v-list-item-content>
+            <div class="d-flex">
+              <v-icon class="order-1 px-2">mdi-logout</v-icon>
+              <v-list-item-title class="order-2">LOGOUT</v-list-item-title>
+            </div>
+          </v-list-item-content>
+        </v-list>
+      </v-menu>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
 
-  components: {
-    HelloWorld,
-  },
+    data: () => ({
+      items: [
+        { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+        { title: 'Data Member', icon: 'mdi-badge-account-horizontal-outline' },
+        { title: 'Data Kelas', icon: 'mdi-dumbbell' },
+        { title: 'Data Admin', icon: 'mdi-badge-account-horizontal-outline' },
+        { title: 'Classes', icon: 'mdi-dumbbell' },
+        { title: 'Newsletter', icon: 'mdi-newspaper-variant' },
+        { title: 'Logout', icon: 'mdi-logout' },
+      ],
+      drawer: false,
+      group: null,
+      page: "Dashboard",
+    }),
 
-  data: () => ({
-    //
-  }),
-};
+    watch: {
+      group () {
+        this.drawer = false
+      },
+    },
+
+    methods :{
+      press(path){
+        this.page = path;
+      // this.$router.push({name: path})
+    }
+  },
+  }
 </script>
+
+<style>
+.v-main__wrap {
+    background-color: #FEE9CC;
+}
+
+.v-image__image, .v-image__placeholder {
+    max-width: 70%;
+}
+
+* {
+    justify-content: center;
+}
+
+
+
+</style>
---
+++ admin-gym-system/src/views/Dashboard.vue
@@ -0,0 +1,318 @@
+<template>
+    <v-container>
+        <div
+        class="d-flex justify-center mb-6 mt-7"
+        >
+            <v-spacer class="order-1 pa-2"></v-spacer>
+
+            <div class="order-2 pa-2">
+                <v-card
+                    class="text-left mx-auto"
+                    width="400"
+                >
+                    <v-img
+                    class="text-left ml-auto"
+                    height="95px"
+                    >
+                    <div
+                    class="d-flex"
+                    >
+                    <div class="order-1  pl-2">
+                        <v-card-title class="text-left">Jumlah User</v-card-title>
+                        <v-card-subtitle>23</v-card-subtitle>
+                    </div>
+                    <v-spacer class="order-2"></v-spacer>
+                    <v-icon class="order-3 pr-5" size="60">mdi-account-group</v-icon>
+                    </div>
+                    </v-img>
+                </v-card>
+            </div>
+
+            <v-spacer class="order-3 pa-2"></v-spacer>
+
+            <div class="order-4 pa-2">
+               <v-card
+                    class="text-left mx-auto"
+                    width="400"
+                >
+                    <v-img
+                    class="text-left ml-auto"
+                    height="95px"
+                    >
+                    <div
+                    class="d-flex"
+                    >
+                    <div class="order-1  pl-2">
+                        <v-card-title class="text-left">Jumlah Class</v-card-title>
+                        <v-card-subtitle>7</v-card-subtitle>
+                    </div>
+                    <v-spacer class="order-2"></v-spacer>
+                    <v-icon class="order-3 pr-5" size="45">mdi-dumbbell</v-icon>
+                    </div>
+                    </v-img>
+                </v-card>
+            </div>
+
+            <v-spacer class="order-5 pa-2"></v-spacer>
+
+            <div class="order-6 pa-2">
+                <v-card
+                    class="text-left mx-auto"
+                    width="400"
+                >
+                    <v-img
+                    class="text-left ml-auto"
+                    height="95px"
+                    >
+                    <div
+                    class="d-flex"
+                    >
+                    <div class="order-1  pl-2">
+                        <v-card-title class="text-left">Jumlah Feed</v-card-title>
+                        <v-card-subtitle>17</v-card-subtitle>
+                    </div>
+                    <v-spacer class="order-2"></v-spacer>
+                    <v-icon class="order-3 pr-5" size="45">mdi-newspaper-variant-outline</v-icon>
+                    </div>
+                    </v-img>
+                </v-card>
+            </div>
+            <v-spacer class="order-7 pa-2"></v-spacer>
+        </div>
+        <div
+        class="d-flex justify-center mb-6 mt-7"
+        >
+            <v-spacer class="order-1 pa-2"></v-spacer>
+
+            <div class="order-2 pa-2">
+                <v-card
+                    class="text-left mx-auto"
+                    width="400"
+                >
+                    <v-img
+                    class="text-left ml-auto"
+                    height="95px"
+                    >
+                    <div
+                    class="d-flex"
+                    >
+                    <div class="order-1  pl-2">
+                        <v-card-title class="text-left">User Yang Login Hari Ini</v-card-title>
+                        <v-card-subtitle>1</v-card-subtitle>
+                    </div>
+                    <v-spacer class="order-2"></v-spacer>
+                    <v-icon class="order-3 pr-5" size="60">mdi-account-group</v-icon>
+                    </div>
+                    </v-img>
+                </v-card>
+            </div>
+
+            <v-spacer class="order-3 pa-2"></v-spacer>
+
+            <div class="order-4 pa-2">
+               <v-card
+                    class="text-left mx-auto"
+                    width="400"
+                >
+                    <v-img
+                    class="text-left ml-auto"
+                    height="95px"
+                    >
+                    <div
+                    class="d-flex"
+                    >
+                    <div class="order-1  pl-2">
+                        <v-card-title class="text-left">Jumlah Booking Class</v-card-title>
+                        <v-card-subtitle>23</v-card-subtitle>
+                    </div>
+                    <v-spacer class="order-2"></v-spacer>
+                    <v-icon class="order-3 pr-5" size="45">mdi-dumbbell</v-icon>
+                    </div>
+                    </v-img>
+                </v-card>
+            </div>
+
+            <v-spacer class="order-5 pa-2"></v-spacer>
+
+            <div class="order-6 pa-2">
+                <v-card
+                    class="text-left mx-auto"
+                    width="400"
+                >
+                    <v-img
+                    class="text-left ml-auto"
+                    height="95px"
+                    >
+                    <div
+                    class="d-flex"
+                    >
+                    <div class="order-1  pl-2">
+                        <v-card-title class="text-left">Jumlah Trainer</v-card-title>
+                        <v-card-subtitle>10</v-card-subtitle>
+                    </div>
+                    <v-spacer class="order-2"></v-spacer>
+                    <v-icon class="order-3 pr-5" size="45">mdi-account</v-icon>
+                    </div>
+                    </v-img>
+                </v-card>
+            </div>
+            <v-spacer class="order-7 pa-2"></v-spacer>
+        </div>
+        <div
+        class="d-flex justify-center mb-6"
+        >
+            <v-spacer class="order-1 pa-2"></v-spacer>
+
+            <div class="order-2 pa-2">
+                <v-sheet
+                    :color="`grey ${theme.isDark ? 'darken-2' : 'lighten-4'}`"
+                    class="pa-3"
+                >
+                    <v-skeleton-loader
+                    class="mx-auto"
+                    width= "629px"
+                    type="card"
+                    ></v-skeleton-loader>
+                </v-sheet>
+            </div>
+
+            <v-spacer class="order-3 pa-2"></v-spacer>
+
+            <div class="order-4 pa-2">
+               <v-sheet
+                    :color="`grey ${theme.isDark ? 'darken-2' : 'lighten-4'}`"
+                    class="pa-3"
+                >
+                    <v-skeleton-loader
+                    class="mx-auto"
+                    width= "629px"
+                    type="card"
+                    ></v-skeleton-loader>
+                </v-sheet>
+            </div>
+
+            <v-spacer class="order-5 pa-2"></v-spacer>
+        </div>
+        <div
+        class="d-flex justify-center mb-6"
+        >
+            <v-spacer class="order-1 pa-2"></v-spacer>
+
+            <div class="order-2 pa-2">
+                <v-sheet
+                    :color="`grey ${theme.isDark ? 'darken-2' : 'lighten-4'}`"
+                    class="pa-3"
+                >
+                    <v-skeleton-loader
+                    class="mx-auto"
+                    width= "629px"
+                    type="card"
+                    ></v-skeleton-loader>
+                </v-sheet>
+            </div>
+
+            <v-spacer class="order-3 pa-2"></v-spacer>
+
+            <div class="order-4 pa-2">
+               <v-sheet
+                    :color="`grey ${theme.isDark ? 'darken-2' : 'lighten-4'}`"
+                    class="pa-3"
+                >
+                    <v-skeleton-loader
+                    class="mx-auto"
+                    width= "629px"
+                    type="card"
+                    ></v-skeleton-loader>
+                </v-sheet>
+            </div>
+
+            <v-spacer class="order-5 pa-2"></v-spacer>
+        </div>
+    </v-container>
+</template>
+
+<script>
+export default {
+    setup() {
+        
+    },
+    inject: {
+      theme: {
+        default: { isDark: false },
+      },
+    },
+}
+</script>
+
+<style scoped>
+.order-2 .v-image {
+  background-image: url("../assets/frame.jpg");
+}
+
+.order-2 .v-card {
+    border-radius: 8px;
+}
+
+.order-2 .v-card__title{
+    color:#F48743;
+    text-align: left;
+    left: 0;
+}
+.order-2 .v-card__subtitle{
+    color:black;
+    font-size: 16px;
+}
+
+.order-2 .v-icon{
+    color: #FEE9CC;
+    padding-left: 1px;
+}
+
+.order-4 .v-image {
+  background-image: url("../assets/frame.jpg");
+}
+
+.order-4 .v-card {
+    border-radius: 8px;
+}
+
+.order-4 .v-card__title{
+    color:#F48743;
+    text-align: left;
+    left: 0;
+}
+
+.order-4 .v-card__subtitle{
+    color:black;
+    font-size: 16px;
+}
+
+.order-4 .v-icon{
+    color: #FEE9CC;
+    padding-left: 1px;
+}
+
+.order-6 .v-image {
+  background-image: url("../assets/frame.jpg");
+}
+
+.order-6 .v-card {
+    border-radius: 8px;
+}
+
+.order-6 .v-card__title{
+    color:#F48743;
+    text-align: left;
+    left: 0;
+}
+
+.order-6 .v-card__subtitle{
+    color:black;
+    font-size: 16px;
+}
+
+.order-6 .v-icon{
+    color: #FEE9CC;
+    padding-left: 1px;
+}
+</style>
