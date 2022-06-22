3071b94d62ba7d14656580eceaf75d5c5fe0ead2
ichsansurya <ichsansuryad@gmail.com>
2022-06-22 19:08:49 +0700

    tambah form untuk registrasi member

---
+++ admin-gym-system/src/views/RegistrasiMember.vue
@@ -0,0 +1,137 @@
+<template>
+    <v-app id="login">
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="12" sm="8" md="8" >
+            <v-card class="elevation-12">
+                    <v-col cols="12" md="8">
+                        <h3 class="text-left ms-3 mt-4 blue--text">Edit Data User</h3>
+                      <v-card-text class="mt-5">
+                        <!-- <h1
+                          class="text-center display-2 orange--text text--accent-3"
+                        >Fitness Gym</h1> -->
+                        <v-form>
+                            <h4>Nama Lengkap</h4> <br>
+                          <v-text-field
+                            label="Masukkan Nama Lengkap"
+                            name="namalengkap"
+                            type="text"
+                            outlined
+                            color="orange"
+                            max-width="100px"
+                          />
+                            <h4>Email</h4> <br>
+                          <v-text-field
+                            label="Masukkan Email"
+                            name="email"
+                            type="text"
+                            outlined
+                            color="orange"
+                            max-width="100px"
+                          />
+                          <h4>Tanggal Lahir</h4> <br>
+                          <v-text-field
+                            id="tanggallahir"
+                            name="date"
+                            type="date"
+                            outlined
+                            color="orange"
+                          />
+                          <h4>Jenis Kelamin</h4>
+                          <v-radio-group
+                            v-model="row"
+                            row
+                            >
+                            <v-radio
+                                label="Laki-laki"
+                                value="radio-1"
+                            ></v-radio>
+                            
+                            <v-radio
+                                label="Perempuan"
+                                value="radio-2"
+                            ></v-radio>
+                            </v-radio-group>
+                            <h4>Nomor Ponsel</h4> <br>
+                          <v-text-field
+                            label="Masukkan Nomor Ponsel"
+                            name="nomorponsel"
+                            type="text"
+                            outlined
+                            color="orange"
+                            max-width="100px"
+                          />
+
+                          <h4>Alamat</h4> <br>
+                          <v-textarea
+                            outlined
+                            name="alamat"
+                            
+                            ></v-textarea>
+                          <h4>Status</h4> <br>
+                            <v-select
+                            :items="items"
+                            outlined
+                            color="orange"
+                            ></v-select>
+                        </v-form>
+                      </v-card-text>
+                      <div class="text-left mt-5 ms-3">
+                        <v-dialog
+                          v-model="dialog"
+                          persistent
+                          max-width="500"
+                        >
+                          <template v-slot:activator="{ on, attrs }">
+                            <v-btn
+                              color="#F48743"
+                              dark
+                              width="200px"
+                              v-bind="attrs"
+                              v-on="on"
+                            >
+                              Save
+                            </v-btn>
+                          </template>
+                          <v-card>
+                            <v-card-title class="justify-center"><strong>
+                              KONFIRMASI
+                              </strong></v-card-title><br>
+                            <v-card-text>Apakah data yang anda masukkan sudah benar?</v-card-text>
+                            <v-card-actions class="justify-center">
+                              <v-btn
+                                color="error"
+                                @click="dialog = false;"
+                              >
+                                Batal
+                              </v-btn>
+                              <v-btn
+                                color="success"
+                                
+                                @click="dialog = false;"
+                              >
+                                Ya
+                              </v-btn>
+                            </v-card-actions>
+                          </v-card>
+                        </v-dialog>
+                      </div> <br>
+                      <br>
+                    </v-col>
+            </v-card> <br>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+    data: () => ({
+      items: ['Admin Operasional', 'Member'],
+      dialog: false,
+    }),
+}
+</script>
