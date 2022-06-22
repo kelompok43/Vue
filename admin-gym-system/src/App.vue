d5b6515f2b34afb0b8e16d2cdf568f8d2bcecbb1
ichsansurya <99705689+ichsansurya@users.noreply.github.com>
2022-06-22 20:38:04 +0700

    Update App.vue

--- admin-gym-system/src/App.vue
+++ admin-gym-system/src/App.vue
@@ -14,18 +14,14 @@ export default {
 </script>
 
 <style>
-<<<<<<< HEAD
+
 * {
   font-family: 'Poppins', sans-serif !important;
   font-weight: 600;
-=======
+
 .v-main__wrap {
     background-color: #FEE9CC;
->>>>>>> f6db956c8984da0eb68835aa7e4a16f1976f6b37
+
 }
 
-* {
-    justify-content: center;
-    font-family: 'Poppins', sans-serif !important;
-}
 </style>
