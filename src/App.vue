fc468e1e82c592a8b8f8d12954b7b815dc9b0de0
Parni Handayani <64381450+parnihandayani@users.noreply.github.com>
2022-07-02 10:33:48 +0700

    datapelatih

---
+++ src/App.vue
@@ -0,0 +1,119 @@
+<template>
+  <v-app>
+    <v-navigation-drawer 
+      v-model="drawer"
+      fixed
+      app
+      color="#FEFEFE"
+    >
+      <v-list-item>
+        <div class="d-flex align-center pa-52px">
+          <v-list-item-content>
+            <v-img
+            alt="Gym Management Logo"
+            contain
+            src="@/assets/Logo.png"
+            height="127px"
+            width="212px"
+          />
+          </v-list-item-content>
+        </div>
+      </v-list-item>
+
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+          @click="press(item.title)"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    
+    <v-app-bar
+      app
+      color="#FEFEFE"
+      fixed
+    >
+    <v-app-bar-nav-icon 
+      @click.stop="drawer = !drawer"
+      color="#F48743">
+    </v-app-bar-nav-icon>
+
+      <div class="d-flex align-center">
+        <v-app-bar-title
+        class="font-weight-bold"
+        > GYM MEMBERSHIP SYSTEM </v-app-bar-title>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+    data: () => ({
+      items: [
+        { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+        { title: 'Data Admin', icon: 'mdi-badge-account-horizontal-outline' },
+        { title: 'Data Member', icon: 'mdi-badge-account-horizontal-outline' },
+        { title: 'Classes', icon: 'mdi-dumbbell' },
+        { title: 'Newsletter', icon: 'mdi-newspaper-variant' },
+        { title: 'Logout', icon: 'mdi-logout' },
+      ],
+      drawer: false,
+      group: null,
+    }),
+
+    watch: {
+      group () {
+        this.drawer = false
+      },
+    },
+  }
+</script>
+
+<style>
+.v-main__wrap {
+    background-color: #FEE9CC;
+}
+
+.v-image__image, .v-image__placeholder {
+    max-width: 70%;
+}
+
+* {
+    justify-content: center;
+}
+
+
+
+</style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,195 @@
+<template>
+    <v-container>
+        <div
+        class="d-flex justify-center mb-6 mt-7"
+        >
+            <v-spacer class="order-1 pa-2"></v-spacer>
+
+            <div class="order-2 pa-2">
+                <v-sheet
+                    :color="`grey ${theme.isDark ? 'darken-2' : 'lighten-4'}`"
+                    class="pa-3"
+                >
+                    <v-skeleton-loader
+                    class="mx-auto"
+                    width="411px"
+                    height="100px"
+                    type="card"
+                    ></v-skeleton-loader>
+                </v-sheet>
+            </div>
+
+            <v-spacer class="order-3 pa-2"></v-spacer>
+
+            <div class="order-4 pa-2">
+               <v-sheet
+                    :color="`grey ${theme.isDark ? 'darken-2' : 'lighten-4'}`"
+                    class="pa-3"
+                >
+                    <v-skeleton-loader
+                    class="mx-auto"
+                    width="411px"
+                    height="100px"
+                    type="card"
+                    ></v-skeleton-loader>
+                </v-sheet>
+            </div>
+
+            <v-spacer class="order-5 pa-2"></v-spacer>
+
+            <div class="order-6 pa-2">
+                <v-sheet
+                    :color="`grey ${theme.isDark ? 'darken-2' : 'lighten-4'}`"
+                    class="pa-3"
+                >
+                    <v-skeleton-loader
+                    class="mx-auto"
+                    width="411px"
+                    height="100px"
+                    type="card"
+                    ></v-skeleton-loader>
+                </v-sheet>
+            </div>
+            <v-spacer class="order-7 pa-2"></v-spacer>
+        </div>
+        <div
+        class="d-flex justify-center mb-6"
+        >
+            <v-spacer class="order-1 pa-2"></v-spacer>
+
+            <div class="order-2 pa-2">
+                <v-sheet
+                    :color="`grey ${theme.isDark ? 'darken-2' : 'lighten-4'}`"
+                    class="pa-3"
+                >
+                    <v-skeleton-loader
+                    class="mx-auto"
+                    width="411px"
+                    height="100px"
+                    type="card"
+                    ></v-skeleton-loader>
+                </v-sheet>
+            </div>
+
+            <v-spacer class="order-3 pa-2"></v-spacer>
+
+            <div class="order-4 pa-2">
+               <v-sheet
+                    :color="`grey ${theme.isDark ? 'darken-2' : 'lighten-4'}`"
+                    class="pa-3"
+                >
+                    <v-skeleton-loader
+                    class="mx-auto"
+                    width="411px"
+                    height="100px"
+                    type="card"
+                    ></v-skeleton-loader>
+                </v-sheet>
+            </div>
+
+            <v-spacer class="order-5 pa-2"></v-spacer>
+
+            <div class="order-6 pa-2">
+                <v-sheet
+                    :color="`grey ${theme.isDark ? 'darken-2' : 'lighten-4'}`"
+                    class="pa-3"
+                >
+                    <v-skeleton-loader
+                    class="mx-auto"
+                    width="411px"
+                    height="100px"
+                    type="card"
+                    ></v-skeleton-loader>
+                </v-sheet>
+            </div>
+            <v-spacer class="order-7 pa-2"></v-spacer>
+        </div>
+        <div
+        class="d-flex justify-center mb-6"
+        >
+            <v-spacer class="order-1 pa-2"></v-spacer>
+
+            <div class="order-2 pa-2">
+                <v-sheet
+                    :color="`grey ${theme.isDark ? 'darken-2' : 'lighten-4'}`"
+                    class="pa-3"
+                >
+                    <v-skeleton-loader
+                    class="mx-auto"
+                    width= "629px"
+                    type="card"
+                    ></v-skeleton-loader>
+                </v-sheet>
+            </div>
+
+            <v-spacer class="order-3 pa-2"></v-spacer>
+
+            <div class="order-4 pa-2">
+               <v-sheet
+                    :color="`grey ${theme.isDark ? 'darken-2' : 'lighten-4'}`"
+                    class="pa-3"
+                >
+                    <v-skeleton-loader
+                    class="mx-auto"
+                    width= "629px"
+                    type="card"
+                    ></v-skeleton-loader>
+                </v-sheet>
+            </div>
+
+            <v-spacer class="order-5 pa-2"></v-spacer>
+        </div>
+        <div
+        class="d-flex justify-center mb-6"
+        >
+            <v-spacer class="order-1 pa-2"></v-spacer>
+
+            <div class="order-2 pa-2">
+                <v-sheet
+                    :color="`grey ${theme.isDark ? 'darken-2' : 'lighten-4'}`"
+                    class="pa-3"
+                >
+                    <v-skeleton-loader
+                    class="mx-auto"
+                    width= "629px"
+                    type="card"
+                    ></v-skeleton-loader>
+                </v-sheet>
+            </div>
+
+            <v-spacer class="order-3 pa-2"></v-spacer>
+
+            <div class="order-4 pa-2">
+               <v-sheet
+                    :color="`grey ${theme.isDark ? 'darken-2' : 'lighten-4'}`"
+                    class="pa-3"
+                >
+                    <v-skeleton-loader
+                    class="mx-auto"
+                    width= "629px"
+                    type="card"
+                    ></v-skeleton-loader>
+                </v-sheet>
+            </div>
+
+            <v-spacer class="order-5 pa-2"></v-spacer>
+        </div>
+    </v-container>
+</template>
+
+<script>
+export default {
+    setup() {
+        
+    },
+    inject: {
+      theme: {
+        default: { isDark: false },
+      },
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
