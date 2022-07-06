bf3ac92578b8034ca57876fb9f78400e01a0cae0
ichsansurya <ichsansuryad@gmail.com>
2022-07-06 16:45:17 +0700

    mengubah code style

--- admin-gym-system/src/App.vue
+++ admin-gym-system/src/App.vue
@@ -14,19 +14,14 @@ export default {
 </script>
 
 <style>
-<<<<<<< HEAD
-.v-main__wrap {
-    background-color: #FEE9CC;
-=======
 
 * {
   font-family: 'Poppins', sans-serif !important;
   font-weight: 600;
+}
 
 .v-main__wrap {
     background-color: #FEE9CC;
-
->>>>>>> d5b6515f2b34afb0b8e16d2cdf568f8d2bcecbb1
 }
 
 </style>
